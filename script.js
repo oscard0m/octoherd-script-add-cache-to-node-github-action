f58830098dd23e5f5bbe19915ffa5c6ef7f31427
Oscar Dominguez <dominguez.celada@gmail.com>
2021-07-02 23:59:57 +0200

    feat: initial version

---
+++ script.js
@@ -0,0 +1,11 @@
+// @ts-check
+
+/**
+ * Add cache parameter to GitHub Actions using setup-node
+ *
+ * @param {import('@octoherd/cli').Octokit} octokit
+ * @param {import('@octoherd/cli').Repository} repository
+ * @param {object} options
+ * @param {string} [options.cache] Select which package manager you want to use for caching
+ */
+export async function script(octokit, repository, { cache }) {}
