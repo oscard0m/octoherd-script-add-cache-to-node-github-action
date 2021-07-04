f08d2b8d4e95997dfa4e4ebe1e204a5b22ab2c57
Oscar Dominguez <dominguez.celada@gmail.com>
2021-07-05 01:20:03 +0200

    feat: Initial version (#1)
    * docs(README): remove WIP note
    
    * build(package): initial version
    
    * build(package): lock file
    
    * build(gitignore): node_modules
    
    * feat: initial version
    
    * docs(README): badges
    
    * docs(README): usage
    
    * ci(release): initial version
    
    * ci(test): initial version
    
    * feat(script): implement logic to modify workflows with setup-node action
    
    * docs(readme): remove --cache command from example to avoid issues pasting the command with a pipe
    
    * refactor(script): remove and rename global variables and add missing jsdoc
    
    * fix(script): if 'with' statement does not exist, create it
    
    * refactor(script): remove label creation when opening a PR
    
    * refactor(script): use for...of instead of for when iterating yaml file
    
    * fix(script): branch has to be created before adding a new git reference to that branch
    
    * refactor(script): remove unused if(!ref) statement
    
    * build(deps): add octokit-plugin-create-pull-request
    
    * feat(script): integrate logic with octokit-plugin-create-pull-request
    
    * fix(script): do not open PR if no cache statement added to any workflow
    
    * feat(script): improve PR body opened by this script

---
+++ script.js
@@ -0,0 +1,138 @@
+// @ts-check
+
+import { composeCreatePullRequest } from "octokit-plugin-create-pull-request";
+import prettier from "prettier";
+import YAML from "yaml";
+
+const { parseDocument } = YAML;
+
+const BRANCH_NAME = "add-cache-to-node-workflows";
+const PATH = ".github/workflows";
+
+/**
+ * Check if a filename is a YAML file
+ *
+ * @param {string} fileName FileName to be tested
+ *
+ * @return {boolean}
+ */
+const isYamlFile = (fileName) => /\.ya?ml$/.test(fileName);
+
+/**
+ * Add cache parameter to GitHub Actions using setup-node
+ *
+ * @param {import('@octoherd/cli').Octokit} octokit
+ * @param {import('@octoherd/cli').Repository} repository
+ * @param {object} options
+ * @param {string} [options.cache] Select which package manager you want to use for caching
+ */
+export async function script(octokit, repository, { cache = "npm" }) {
+  // Global variables used throughout the code
+  const owner = repository.owner.login;
+  const repo = repository.name;
+  const defaultBranch = repository.default_branch;
+
+  // Get all files from .github/workflows folder
+  let files;
+  try {
+    const { data } = await octokit.request(
+      "GET /repos/{owner}/{repo}/contents/{path}",
+      {
+        owner,
+        repo,
+        path: PATH,
+      }
+    );
+
+    files = data;
+  } catch (e) {
+    if (e.status === 404) {
+      octokit.log.warn(`"${PATH}" path not found in ${repository.full_name}`);
+      return;
+    } else {
+      throw e;
+    }
+  }
+
+  if (!Array.isArray(files)) {
+    throw new Error(`"${PATH}" is not a folder in ${repository.full_name}`);
+  }
+
+  const workflowFiles = files.filter((file) => isYamlFile(file.name));
+  const filesToEdit = {};
+
+  workflowFiles.forEach((element) => {
+    filesToEdit[element.path] = ({ content, encoding }) => {
+      const yamlDocument = parseDocument(
+        Buffer.from(content, encoding).toString("utf-8")
+      );
+      const jobs = yamlDocument.get("jobs");
+      let cacheAdded = false;
+
+      for (const { value: job } of jobs.items) {
+        const steps = job.get("steps");
+        for (const step of steps.items) {
+          const stepUses = step.get("uses");
+          const stepWith = step.get("with");
+
+          if (
+            stepUses &&
+            stepUses.includes("actions/setup-node") &&
+            (!stepWith || !stepWith.get("cache"))
+          ) {
+            if (!stepWith) {
+              step.set("with", { cache });
+            } else {
+              stepWith.set("cache", cache);
+            }
+
+            cacheAdded = true;
+          }
+        }
+      }
+
+      return cacheAdded
+        ? prettier.format(yamlDocument.toString(), {
+            parser: "yaml",
+          })
+        : null;
+    };
+  });
+
+  const prCreated = await composeCreatePullRequest(octokit, {
+    owner,
+    repo,
+    title: "ci(workflow): add cache to workflows using actions/setup-node",
+    body: `## Description
+
+Add \`cache\` to workflows using \`actions/setup-node\`
+
+## Context
+
+\`setup-node\` GitHub Action just released a new option to add cache to steps using it.
+
+You can find the details here: https://github.blog/changelog/2021-07-02-github-actions-setup-node-now-supports-dependency-caching/
+
+---
+
+🤖 This PR has been generated automatically by [this octoherd script](https://github.com/oscard0m/octoherd-script-add-cache-to-node-github-action), feel free to run it in your GitHub user/org repositories! 💪🏾
+`,
+    base: defaultBranch,
+    head: BRANCH_NAME,
+    createWhenEmpty: false,
+    changes: [
+      {
+        files: filesToEdit,
+        commit: `ci(workflow): add '${cache}' cache for actions/setup-node in ${PATH}`,
+        emptyCommit: false,
+      },
+    ],
+  });
+
+  if (!prCreated) {
+    octokit.log.warn(`No Pull Request created for ${repository.full_name}`);
+    return;
+  }
+
+  octokit.log.info(`Pull Request created at ${prCreated.data.html_url}`);
+}
